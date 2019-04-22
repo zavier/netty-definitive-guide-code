f0145d29d87e4239f6878c31b8489e6b5a21db4a
zheng <765324639@qq.com>
2019-04-23 00:41:58 +0800

    proto使用demo

---
+++ src/test/java/com/zavier/codec/proto/TestSubscribeReqProto.java
@@ -0,0 +1,36 @@
+package com.zavier.codec.proto;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestSubscribeReqProto {
+    private static byte[] encode(SubscribeReqProto.SubscribeReq req) {
+        return req.toByteArray();
+    }
+
+    private static SubscribeReqProto.SubscribeReq decode(byte[] body) throws InvalidProtocolBufferException {
+        return SubscribeReqProto.SubscribeReq.parseFrom(body);
+    }
+
+    private static SubscribeReqProto.SubscribeReq createSubscribeReq() {
+        SubscribeReqProto.SubscribeReq.Builder builder = SubscribeReqProto.SubscribeReq.newBuilder();
+        builder.setSubReqID(1);
+        builder.setUserName("zavier");
+        builder.setProductName("Netty");
+        List<String> address = new ArrayList<>();
+        address.add("BeiJing");
+        address.add("NanJing");
+        builder.addAllAddress(address);
+        return builder.build();
+    }
+
+    public static void main(String[] args) throws InvalidProtocolBufferException {
+        SubscribeReqProto.SubscribeReq req = createSubscribeReq();
+        System.out.println("Before encode : " + req.toString());
+        SubscribeReqProto.SubscribeReq req2 = decode(encode(req));
+        System.out.println("After decode: " + req2.toString());
+        System.out.println("Assert equal:-->" + req2.equals(req));
+    }
+}
